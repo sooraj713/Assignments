8d87ab4d345adf47bf3ea259af11ecf1f363a8a5
Sooraj Gaikwad <suraj.gaikwad@cuelogic.com>
2021-01-21 18:24:46 +0530

    conflict solved

--- add.c
+++ add.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
+using namespace std;
 int main()
 {
 	int a=10,b=20;
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 	if(a<0) //change negative no to positive number
@@ -12,11 +14,18 @@ int main()
 >>>>>>> parent of ece72ff... convert negative no to positive
 	printf("Addition: %d",add(a,b));
 
+=======
+	printf("Addition: %d",add(a,b));
+>>>>>>> parent of 977fd54... GIT-001 & GIT 002 updated
 	printf("Substraction: %d",sub(a,b));
 	return 0;
 }
 
+<<<<<<< HEAD
 float add(int a, int b)
+=======
+int add(int a, int b)
+>>>>>>> parent of 977fd54... GIT-001 & GIT 002 updated
 {
 	return (a+b);
 }
