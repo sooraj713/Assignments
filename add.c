977fd54cced2f48f6cd14747871891c88e66dfb4
Sooraj Gaikwad <suraj.gaikwad@cuelogic.com>
2021-01-21 11:47:52 +0530

    GIT-001 & GIT 002 updated

--- add.c
+++ add.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
-using namespace std;
 int main()
 {
 	int a=10,b=20;
-	printf("Addition: %d",add(a,b));
+	printf("Addition: %d",addition(a,b));
 	printf("Substraction: %d",sub(a,b));
 	return 0;
 }
 
-int add(int a, int b)
+float addition(int a, int b)
 {
 	return (a+b);
 }
