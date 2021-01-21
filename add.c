20b0283b8a770576d2d4d216aa9b7ba115689941
Sooraj Gaikwad <suraj.gaikwad@cuelogic.com>
2021-01-21 18:17:14 +0530

    add.c merged

--- add.c
+++ add.c
@@ -2,8 +2,12 @@
 int main()
 {
 	int a=10,b=20;
+<<<<<<< HEAD
 
 	printf("Addition: %d",addition(a,b));
+=======
+	printf("Addition: %d",add(a,b));
+>>>>>>> parent of ece72ff... convert negative no to positive
 	printf("Substraction: %d",sub(a,b));
 	return 0;
 }
