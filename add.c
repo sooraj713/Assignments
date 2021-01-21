69a02e2c0312b875e245f546c4201a9164f2f7b5
Sooraj Gaikwad <suraj.gaikwad@cuelogic.com>
2021-01-21 18:37:20 +0530

    Added Devide Function

--- add.c
+++ add.c
@@ -4,12 +4,18 @@ int main()
 	int a=10,b=20;
 	printf("Addition: %d",addition(a,b));
 	printf("Substraction: %d",sub(a,b));
+	printf("devidation": "%d",devide(a,b));
 	return 0;
 }
 
 float addition(int a, int b)
 {
 	return (a+b);
+}
+
+int devide(int a, int b)
+{
+	return (a*b);
 }
 
 int sub(int a, int b)
